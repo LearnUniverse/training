a150edaeaf5c91480128cc9d57b90d17de122670
Pavel Vondruška <pvon@essencebs.com>
2025-03-07 16:56:41 +0100

    FirstReactApp_runnable

---
+++ my-webtoonz/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        FireBAse Course
+        My first WebToons running on Firebase
+      </header>
+    </div>
+  );
+}
+
+export default App;
