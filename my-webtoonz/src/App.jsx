a698563cd81cc53119809169a154cc6351b24a5f
LearnUniverse <vonuniverse@email.com>
2025-03-29 23:15:30 +0100

    bojSeStateArenderJSX

--- my-webtoonz/src/App.jsx
+++ my-webtoonz/src/App.jsx
@@ -1,28 +1,48 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState} from 'react'; 
 
-
-function HandleButtonClick() {
-  alert("Tlačítko bylo stisknuto!");
-}
-
-
-function MyButton() {
+function MyButton({count, onClickMe}) {
   return (
-    <button className="myButton" onClick={HandleButtonClick}>
+    <button className="myButton" onClick={onClickMe}>
       <img src={logo} className="App-logo" alt="logo" />
-      <br/>Moje tlačítko  
+      <br/>Moje tlačítko bylo stisknuto {count} krát!  
     </button>
   );
 }
 
+const optionsList = [
+  {name: 'Option 1', id:1},
+  {name: 'Option 2', id:2}, 
+  {name: 'Option 3', id:3}
+];
+
+const listItems = optionsList.map(item =>
+  <li key={item.id}>{item.name}</li>
+);
+
+function MyList() {
+  return (
+    <ul>{listItems}</ul>
+  );
+}
+
 function App() {
+  const [count, setCount] = useState(0);
+
+  function HandleButtonClick() {
+    setCount(count + 1);
+    alert("Tlačítko bylo stisknuto!");
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         FireBAse Course
-        My first {MyButton()} WebToons running on Firebase
+        My first <MyButton count={count} onClickMe = {HandleButtonClick}/>  and second 
+        <MyButton count={count} onClickMe = {HandleButtonClick}/> WebToons running on Firebase
+        <MyList />
       </header>
     </div>
   );
