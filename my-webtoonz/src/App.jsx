28c0d24bb592a952698ae8cf331bd9bf0ae18874
LearnUniverse <vonuniverse@email.com>
2025-04-06 12:07:36 +0200

    BeforeRemoveStages_Game

--- my-webtoonz/src/App.jsx
+++ my-webtoonz/src/App.jsx
@@ -27,6 +27,15 @@ function MyList() {
   );
 }
 
+const game = [{id: "G1",name: "The ONE",sequence: ""},
+              {id: "G2",name: "The TWO",sequence: "G1"}, 
+              {id: "G3",name: "The THREE",sequence: "G2"}];
+
+const gameStages = [{gageId:"G1",id: "S1",name:"G1: First Stage",content:"<p>G1: First Stage</p>"},
+                    {gageId:"G1",id: "S2",name:"G2: Second Stage",content:"<p>G2: Second Stage</p>"}, 
+                    {gageId:"G1",id: "S3",name:"G3: Third Stage",content:"<p>G3: Third Stage</p>"},];
+
+
 function App() {
   const [count, setCount] = useState(0);
 
